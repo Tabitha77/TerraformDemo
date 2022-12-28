b62ff43f19cd668eb5fd42cff900f3b37170788d
Tabitha77 <109340849+Tabitha77@users.noreply.github.com>
2022-12-28 23:18:12 +0530

    Update main.tf

--- app-service-variables/main.tf
+++ app-service-variables/main.tf
@@ -29,20 +29,18 @@ resource "azurerm_service_plan" "app_service_plan_terraform" {
   }
 }
 
-resource "azurerm_app_service" "app_service_terraform" {
+  
+  resource "azurerm_linux_web_app" "app_service_terraform" {
   name                = "app-service-terraform-houssem-4"
-  location            = azurerm_resource_group.resource_group_terraform.location
-  resource_group_name = azurerm_resource_group.resource_group_terraform.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan_terraform.id
+  resource_group_name = azurerm_resource_group.RG-Terraform.name
+  location            = azurerm_resource_group.RG-Terraform.location
+  service_plan_id     = azurerm_app_service_plan.ASP-TerraForm.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
+  dotnet_framework_version = "v4.0"
+   scm_type                 = "LocalGit"
   }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
+}
 
   connection_string {
     name  = "Database"
